822e58e5abf2d508d290f954ae47a3abf5e58b60
Luke Bussey <luke@loganbussey.com>
2024-05-08 09:54:04 -0700

    Format everything

--- apps/backend/daemon.js
+++ apps/backend/daemon.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 
-const { spawn } = require("child_process");
+const { spawn } = require('child_process');
 
 // Start the supabase process
-const supabase = spawn("npx", ["supabase", "start"]);
+const supabase = spawn('npx', ['supabase', 'start']);
 
 // Handle the output
-supabase.stdout.on("data", (data) => {
+supabase.stdout.on('data', (data) => {
   process.stdout.write(`${data}`);
 });
 
-supabase.stderr.on("data", (data) => {
+supabase.stderr.on('data', (data) => {
   process.stdout.write(`${data}`);
 });
 
@@ -19,13 +19,13 @@ process.stdin.resume();
 
 // Handle the SIGINT once
 (async () => {
-  await new Promise((resolve) => process.on("SIGINT", resolve));
+  await new Promise((resolve) => process.on('SIGINT', resolve));
 
-  process.stdout.write("Stopping supabase...");
+  process.stdout.write('Stopping supabase...');
 
-  const stop = spawn("npx", ["supabase", "stop"]);
+  const stop = spawn('npx', ['supabase', 'stop']);
 
-  stop.on("close", (code) => {
+  stop.on('close', (code) => {
     process.stdout.write(`Supabase stopped.`);
     process.exit(0);
   });
